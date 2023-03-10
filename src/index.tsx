03a311fb173007cf38e26f3d0a151899be89dd40
Jason Thorpe <jathorpe@gmail.com>
2023-03-10 14:33:11 -0800

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+import { DetailsList } from '@fluentui/react/lib/DetailsList';
+
+function App() {
+  const columns = [
+    {
+      key: "column1",
+      name: "Name",
+      fieldName: "name",
+      minWidth: 100,
+      maxWidth: 200,
+      isResizable: true,
+    },
+    {
+      key: "column2",
+      name: "Value",
+      fieldName: "value",
+      minWidth: 100,
+      maxWidth: 200,
+      isResizable: true,
+    },
+  ];
+  const items = [
+    { key: 1, name: "good", value: 1 },
+    { key: 2, name: "bad", value: 2 },
+  ];
+  return (
+    <div className="App">
+      <DetailsList items={items} columns={columns} setKey="set" />
+    </div>
+  )
+}
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <div>
+
+    <h1>No Strict Mode</h1>
     <App />
-  </React.StrictMode>
+    <h1>Strict Mode Applied</h1>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
